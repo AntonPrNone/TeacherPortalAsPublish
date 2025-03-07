14775d47d059e3afb1a4695f0feefb7a47703a5d
Антон <yznikfortyni@gmail.com>
2025-03-07 22:35:53 +0300

    правка

--- js/site.js
+++ js/site.js
@@ -5,4 +5,18 @@ window.resizeImage = function (element, maxWidth, maxHeight) {
         img.style.width = (img.naturalWidth * ratio) + 'px';
         img.style.height = (img.naturalHeight * ratio) + 'px';
     }
+};
+
+// Функция для анимации изменения вида
+window.animateViewChange = function (viewId) {
+    const view = document.getElementById(viewId);
+    if (view) {
+        view.style.opacity = '0';
+        view.style.transform = 'translateY(10px)';
+        
+        setTimeout(() => {
+            view.style.opacity = '1';
+            view.style.transform = 'translateY(0)';
+        }, 50);
+    }
 }; 
